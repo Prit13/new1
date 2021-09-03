bc7099301ec21b01f31ac229fd2f30da4eb6f3e6
Prit <75497759+Prit13@users.noreply.github.com>
2021-09-04 01:33:58 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,17 @@
+// package com.company;
+import java.util.Scanner;
+
+public class String {
+    public static void main(String[] args) {
+        Scanner i=new Scanner(System.in);
+
+        System.out.print("Enter 1 word : ");
+        String a=i.next();
+        System.out.println(a);
+
+        System.out.print("Enter multiple words : ");
+        String b=i.nextLine();
+        System.out.println(b);
+
+    }
+}
